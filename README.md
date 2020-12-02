bf35a290b19b970d9a857168472fc3a944cf3672
Barry Ervin <barry.ervin@slalom.com>
2020-12-02 16:57:13 -0500

    Refactored

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Here are my solutions for AoC2020
 
 Use the template for the daySetup to start your new day
 
-Run "npm run today" for teh latest problem
+"npm run day _" with an int in the black to run that day, defaults to latest day
+Run "npm run today" for the latest problem
 Run "npm run all" for all problems
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dayArg = process.argv[1]
 
 function day(){
     dayArg != undefined ? 
-    console.log(days[dayArg-1].main(),"\n")
+    console.log(`Day ${dayArg}\n${days[dayArg-1].main()}\n`)
     :
     console.log(days[days.length-1].main(),"\n")
 }
@@ -15,7 +15,7 @@ function today(){
 
 function all(){
     for (let i = 0; i < days.length; i++) {
-        console.log(days[i].main(),"\n")  
+        console.log(`Day ${i+1}\n${days[i].main()}\n`)  
     }
 }
 
