2631b247d44bc37cfb04aca4cc792d6931520339
Barry Ervin <barry.ervin@slalom.com>
2020-12-03 01:04:41 -0500

    Third complete

--- Days/3.js
+++ Days/3.js
@@ -1,11 +1,12 @@
 const utils = require('../Utils')
 
 function main () {
-    data = utils.parseData(3);
-    return `${partOne.name}: ${partOne(data)}\n${partTwo.name}: ${partTwo(data)}`
+    const data = utils.parseData(3);
+    const slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+    return `${oneSlope.name}: ${oneSlope(data,slopes[1])}\n${manySlopes.name}: ${manySlopes(data,slopes)}`
 };
 
-function partOne(data){ 
+function oneSlope(data,slope){ 
     let row = 0;
     let column = 0;
     let count = 0;
@@ -15,14 +16,14 @@ function partOne(data){
             column = column%data[0].length
         }
         if(data[row][column]==='#'){count++}
-        row+=1
-        column+=3
+        row+=slope[1]
+        column+=slope[0]
     }
 
     return count;
 }
 
-function partTwo(data){
+function manySlopes(data){
     const slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
     let count = 1
 
@@ -38,15 +39,14 @@ function partTwo(data){
             row+=slope[1]
             column+=slope[0]
         }
-        console.log(curr)
         count *= curr;
     })
-    
+
     return count;
 }
 
 module.exports = {
     main,
-    partOne,
-    partTwo
+    oneSlope,
+    manySlopes
 }
