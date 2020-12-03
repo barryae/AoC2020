46172b4d2aa728139662e3df1c5b15caf5fe73e7
Barry Ervin <barry.ervin@slalom.com>
2020-12-03 12:33:41 -0500

    Refactored 3 to have one function

--- Days/3.js
+++ Days/3.js
@@ -3,50 +3,33 @@ const utils = require('../Utils')
 function main () {
     const data = utils.parseData(3);
     const slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
-    return `${oneSlope.name}: ${oneSlope(data,slopes[1])}\n${manySlopes.name}: ${manySlopes(data,slopes)}`
+    return `Part One: ${countTrees(data,[slopes[1]])}\nPart Two: ${countTrees(data,slopes)}`
 };
 
-function oneSlope(data,slope){ 
-    let row = 0;
-    let column = 0;
-    let count = 0;
+function countTrees(data,slopes){
+    let answer = 1
 
-    while(row<data.length){
-        if(column>data[0].length-1){
-            column = column%data[0].length
-        }
-        if(data[row][column]==='#'){count++}
-        row+=slope[1]
-        column+=slope[0]
-    }
-
-    return count;
-}
-
-function manySlopes(data){
-    const slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
-    let count = 1
+    // If single slope is given, wrap in array
+    if(slopes[0][0]==undefined){slopes=[slopes]}
 
     slopes.forEach((slope)=>{
         let row = 0;
         let column = 0;
-        let curr = 0;
+        let count = 0;
         while(row<data.length){
-            if(column>data[0].length-1){
+            if(column>=data[0].length){
                 column = column%data[0].length
             }
-            if(data[row][column]==='#'){curr++}
+            if(data[row][column]==='#'){count++}
             row+=slope[1]
             column+=slope[0]
         }
-        count *= curr;
+        answer *= count;
     })
-
-    return count;
+    return answer;
 }
 
 module.exports = {
     main,
-    oneSlope,
-    manySlopes
+    countTrees
 }
